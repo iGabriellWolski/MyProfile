f8878b2aed331c7312d4915c921ed32a828207aa
Gabriel Wolski <igabriell.wolski@gmail.com>
2024-02-03 04:48:28 -0300

    finalizando o projeto

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,5 +1,6 @@
 const sections = document.querySelectorAll(".js-anima-scroll");
 const windowMetade = window.innerHeight * 0.6;
+
 function animaScroll() {
   sections.forEach((sessao) => {
     const sectionTop = sessao.getBoundingClientRect().top - windowMetade;
@@ -14,3 +15,16 @@ function animaScroll() {
 animaScroll();
 
 window.addEventListener("scroll", animaScroll);
+
+const menuLinks = document.querySelectorAll(".js-link_menu");
+
+function linkAtivo() {
+  menuLinks.forEach((link) => {
+    link.classList.remove("ativo");
+  });
+  this.classList.add("ativo");
+}
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", linkAtivo);
+});

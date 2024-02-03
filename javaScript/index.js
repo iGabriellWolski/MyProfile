22fcb8619443acae36358d7d61b6c9ba31a7a311
Gabriel Wolski <igabriell.wolski@gmail.com>
2024-02-02 23:41:11 -0300

    adicinando design retrô

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,17 +1,16 @@
 const sections = document.querySelectorAll(".js-anima-scroll");
-const windowMetade = window.innerHeight * 0.6
-    function animaScroll(){
-        sections.forEach((sessao) =>{
-            const sectionTop = sessao.getBoundingClientRect().top - windowMetade
-            console.log(sectionTop);
-            if( sectionTop < 0){
-                sessao.classList.add("ativo")
-            }else{
-                sessao.classList.remove("ativo")
-            }
-        })
+const windowMetade = window.innerHeight * 0.6;
+function animaScroll() {
+  sections.forEach((sessao) => {
+    const sectionTop = sessao.getBoundingClientRect().top - windowMetade;
+    if (sectionTop < 0) {
+      sessao.classList.add("ativo");
+    } else {
+      sessao.classList.remove("ativo");
     }
+  });
+}
 
-    animaScroll()
+animaScroll();
 
-    window.addEventListener("scroll",animaScroll);
+window.addEventListener("scroll", animaScroll);
